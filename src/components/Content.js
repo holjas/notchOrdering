84d9c5940059dd231e5657129a1d8ee9a3563008
holjas <holly.jasiura@gmail.com>
2021-06-30 13:08:56 -0400

    filter working

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,39 +1,54 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 // delete testdata
 //vring in axios and useeffect from app.js
 import testdata from "../response.json";
-
 
 import Filters from "./Filters";
 import OrderList from "./OrdersList";
 
 const Content = () => {
-    const [orderList, setOrderList] = useState([]);
-    const [vendor, setVendorName] = useState("allSuppliers")
+  const [orderList, setOrderList] = useState([]);
+  const [vendor, setVendorName] = useState("");
+  const [sortedVendor, setSortedVendor] = useState([]);
 
-    //testing only. using local data
-    useEffect(()=> {
-      setOrderList(testdata)
-      console.log("use effect fired")
-    },[])
+  //testing only.!!!!!! using local data
+  useEffect(() => {
+    setOrderList(testdata.data);
+    setSortedVendor(testdata.data);
+  }, []);
 
-console.log("test data", testdata);
-console.log("iam orderlist", orderList)
-console.log("vendor= ", vendor)
-    
-//capture vendor selected
-const handleSelect = (e) => {
+  //capture vendor selected
+  const handleSelect = (e) => {
     setVendorName(e.target.value);
-    console.log(e.target.value)
- }
+    // console.log(e.target.value);
+  };
 
+  //view order list by selected supplier
+  useEffect(() => {
+    // console.log("new use efffect fired");
+    if (vendor) {
+      setVendorName(vendor);
+      // console.log("new vendor name", vendor);
+      const mything = orderList.filter((order) => order.vendorName === vendor);
+      setSortedVendor(mything);
+    }
+    // if (vendor === "allSupplier") {
+    //   setSortedVendor(orderList);
+    // }
+  }, [vendor, orderList]);
 
-    return ( 
-        <>
-        <Filters vendors={orderList.data} handleSelect={handleSelect}/>
-        <OrderList orderData={orderList.data}/>
-        </>
-     );
-}
- 
+  console.log("Orderlist ", orderList);
+  console.log("sortedvendor", sortedVendor);
+  // console.log("test data", testdata);
+  // console.log("iam orderlist", orderL#2d0d6dist)
+  // console.log("vendor= ", vendor)
+
+  return (
+    <>
+      <Filters vendors={orderList} handleSelect={handleSelect} />
+      <OrderList orderData={sortedVendor} />
+    </>
+  );
+};
+
 export default Content;
